f7e6519d257e39ef2fe0d112100149a2d8552390
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-13 12:46:04 -0800

    Looping through athletesArray contents

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
  var athletesArray = ["Kobe Bryant", "Michael Jordan", "Lebron James", "Tom Brady"];
-    
+    for (var i = 0; i < athletesArray.length; i++) {
+        $("#athlete-buttons").append("<button type='button' onclick='searchGif(\"" + athletesArray[i] + "\")' class='btn btn-primary' value=' " + athletesArray[i] + "'> " + athletesArray[i] + " </button>");
+    }
     }
     
