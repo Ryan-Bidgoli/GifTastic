ce3455e04b44eec8a9ef6981469fe0b03553c23a
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-13 22:44:32 -0800

    Accessing object to animate gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,7 +42,17 @@ function displayGif(response) {
         image = '<div class="col-md-4">' + image + "</div>";
         $('#athletes').append(image);
     }
- 
+  $('.movImage').on('click', function() {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).attr("data-animate"));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).attr("data-still"));
+            $(this).attr('data-state', 'still');
+        }
+
+    });
    
 
 }
