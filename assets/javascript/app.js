20751116578e0d52f3f826050b5a44897bc1021f
Ryan-Bidgoli <52834367+Ryan-Bidgoli@users.noreply.github.com>
2019-11-13 22:33:29 -0800

    Looping through Gif images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,7 +30,19 @@ function searchGif(gifName) {
         })
 }
 
-function displayGif(response) {
-    
-}
 
+function displayGif(response) {
+    $('#athletes').empty();
+    for (var i = 0; i < response.data.length; i++) {
+        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+        var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
+            '" data-still=" ' + response.data[i].images.fixed_height_still.url +
+            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:250px; height:250px">';
+
+        image = '<div class="col-md-4">' + image + "</div>";
+        $('#athletes').append(image);
+    }
+ 
+   
+
+}
